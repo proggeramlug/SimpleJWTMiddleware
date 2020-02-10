28962680dd616394db0a6bc0b726e8e53383df32
Ralph Kuepper <ralph.kuepper@skelpo.com>
2020-02-09 21:42:04 -0500

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "SimpleJWTMiddleware",
+    platforms: [
+       .macOS(.v10_15)
+    ],
+    products: [
+        .library(name: "SimpleJWTMiddleware", targets: ["SimpleJWTMiddleware"]),
+    ],
+    dependencies: [
+    .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.3"),
+    .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0-beta.2"),
+    ],
+    targets: [
+        .target(name: "SimpleJWTMiddleware", dependencies: ["Vapor", "JWT"]),
+        .testTarget(name: "SimpleJWTMiddlewareTests", dependencies: ["SimpleJWTMiddleware"])
+    ]
+)
---
+++ Tests/JWTMiddlewareTests/JWTMiddlewareTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+@testable import JWTMiddleware
+
+final class JWTMiddlewareTests: XCTestCase {
+    func testExample() {}
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
