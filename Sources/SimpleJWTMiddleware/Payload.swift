9caf2f66d3bca3ea5f486d2f57ff2b96664aa45b
Ralph Kuepper <ralph.kuepper@skelpo.com>
2020-02-20 20:01:04 -1000

    Update

--- Sources/SimpleJWTMiddleware/Payload.swift
+++ Sources/SimpleJWTMiddleware/Payload.swift
@@ -6,16 +6,14 @@ public struct Payload: JWTPayload {
     public let firstname: String?
     public let lastname: String?
     public let email: String
-    public let role: String
     public let id: Int
     public let status: Int = 0
     public let exp: String
     public let iat: String
     
-    init(id: Int, email: String, role: String) {
+    init(id: Int, email: String) {
         self.id = id
         self.email = email
-        self.role = role
         self.firstname = nil
         self.lastname = nil
         self.exp = String(Date().addingTimeInterval(60*60*24).timeIntervalSince1970)
