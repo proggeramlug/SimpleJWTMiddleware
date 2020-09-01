961ab653165f3b3e9c7351d922623aab293a6143
Ralph Küpper <ralph.kuepper@skelpo.com>
2020-09-01 15:02:43 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0"),
     ],
     targets: [
         .target(name: "SimpleJWTMiddleware", dependencies: ["Vapor", "JWT"])
